23664bf3f0f40f3673b7e31014e89562f4388d68
Федорук Денис <nightsin08@gmail.com>
2024-05-05 13:31:58 +0500

    fix gh-pages

--- src/components/card/card.data.tsx
+++ src/components/card/card.data.tsx
@@ -6,49 +6,49 @@ export interface ICardData {
 }
 
 export const CopperData:ICardData[] = [{
-    url:"../../../public/assets/Copper_1.png",
+    url:"/assets/Copper_1.png",
     cost:14890,
     title:"Комплект Заземления Омедненный 6м",
     count: 1
     },
     {
-        url:"../../../public/assets/Copper_1.png",
+        url:"/assets/Copper_1.png",
         cost:28990,
         title:"Комплект Заземления Омедненный 9м",
         count: 1
     },
     {
-        url:"../../../public/assets/copper_15m.png",
+        url:"/assets/copper_15m.png",
         cost:59990,
         title:"Комплект Заземления Омедненный 15м",
         count: 1
     },
     {
-        url:"../../../public/assets/ground_rods.jpg",
+        url:"/assets/ground_rods.jpg",
         cost:14700,
         title:"Стержни Заземления Омедненные",
         count: 1
-    },
+    }
 ]
 export const GalvanizedData:ICardData[] = [
     {
-        url:"../../../public/assets/galvanized_6m.jpg",
+        url:"/assets/galvanized_6m.jpg",
         cost:16800,
         title:"Комплект Заземления Оцинкованный 6м",
         count: 1
     },
     {
-        url:"../../../public/assets/galvanized_6m.jpg",
+        url:"/assets/galvanized_6m.jpg",
         cost:32990,
         title:"Комплект Заземления Оцинкованный 9м",
         count: 1
     },{
-        url:"../../../public/assets/galvanized_9m.jpg",
+        url:"/assets/galvanized_9m.jpg",
         cost:45600,
         title:"Комплект Заземления Оцинкованный 15м ",
         count: 1
     },{
-        url:"../../../public/assets/galvanized_rods.jpg",
+        url:"/assets/galvanized_rods.jpg",
         cost:14990,
         title:"Cтержни Заземления Оцинкованные",
         count: 1
